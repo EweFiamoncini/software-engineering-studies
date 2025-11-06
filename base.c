11950a86908261ef71a1bbe9d9e018c83a5a5f6a
Ewe Fiamoncini <ewertonfiamoncini20@gmail.com>
2025-11-06 10:18:20 -0300

    Implement code changes to enhance functionality and improve performance

---
+++ base.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World! Program developed in VS Code.\n");
+    printf("Hello, World! Program developed in VS Code.\n");
+    printf("Hello, World! Program developed in VS Code.\n");
+    return 0;
+}
