71a9cc2625434779e0c4930a101f0c69d630235e
Ewe Fiamoncini <ewertonfiamoncini20@gmail.com>
2025-11-06 10:56:41 -0300

    Refactor base.c for clarity and add VS Code settings for C file associations

--- base.c
+++ base.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+/*First program in C*/
 
-int main()
-{
-    printf("Hello, World! Program developed in VS Code.\n");
-    printf("Hello, World! Program developed in VS Code.\n");
-    printf("Hello, World! Program developed in VS Code.\n");
-    return 0;
-}
+#include <stdio.h>/*Esta instrução insere o conteudo do arquivo stdio.h*/
+
+int main()/*Todo programa em C deve possuir essa linha*/
+{/*delimita o inicio das instruções*/
+    printf("Hello, World! Program developed in VS Code.\n");/*Esta instrução imprime uma mensagem na tela!*/
+    return 0;/*Este retorno indica que o programa esta finalizado sem erro*/
+}/*Delimita o fim do conjunto de instruções da função main*/
